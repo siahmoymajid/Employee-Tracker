5c73767a93369d50a9f6108beb1aad7a37f58e8d
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-03-25 17:03:49 -0700

    updated files

---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+USE employees;
+INSERT INTO department (name)
+VALUES ("HR"),
+("test"),
+("test1"); 
+
+INSERT INTO role (name)
+VALUES ("title", "salary","department_id"),
+("test", "test","test",),
+("test1","test2", "test3"); 
+
+INSERT INTO employees(name)
+VALUES ("Bob","Smith",1, null),
+("test", "test","test","test"),
+("test1", "test2", "test3"); 
